bfa2a9b79eb21d093ef39a5b3a3468adf9b3c2d9
karol <karolpasierb@op.pl>
2017-06-27 00:42:09 +0200

    Ajax Joke

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var url = 'http://api.icndb.com/jokes/random';
+
+
+var button = document.getElementById('get-joke');
+button.addEventListener('click', function(){
+  var paragraph = document.getElementById('joke');
+	getJoke() {
+  		var xhr = new XMLHttpRequest();
+  		xhr.open('GET', url);
+  		xhr.addEventListener('load', function(){
+    		var response = JSON.parse(xhr.response);
+    		paragraph.innerHTML = response.value.joke;
+  		});
+  	xhr.send();
+	}
+
+});
